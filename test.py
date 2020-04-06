c7d75212f0b5c93ea12a57154471eee62b23a6a9
Dominic Sciara <dominic.sciara@gmail.com>
2020-04-06 15:40:01 -0400

    built full class and test

---
+++ test.py
@@ -0,0 +1,62 @@
+# Testing Timer class by timing differetn ways of calculating 
+# the fibonacci sequence
+
+
+from timer import Timer
+
+def recursive_fib(n):
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        return recursive_fib(n - 1) + recursive_fib(n - 2)
+
+def memoization_fib(n, mem = {0: 0, 1: 1}):
+    if n in mem:
+        return mem[n]
+    else:
+        mem[n] = memoization_fib(n - 1) + memoization_fib(n - 2)
+        return mem[n]
+
+def iterative_fib(n):
+    last_two = [0, 1]
+     
+    while n > 1:
+        temp = sum(last_two)
+        last_two[0] = last_two[1]
+        last_two[1] = temp
+        n -= 1
+    return last_two[n]
+
+
+
+
+def main():
+    t = Timer()
+
+    print '\n'
+
+    t.start()
+    print recursive_fib(25)
+    t.end()
+
+    print '\n'
+
+    t.start()
+    print memoization_fib(25)
+    t.end()
+
+    print '\n'
+
+    t.start()
+    print iterative_fib(25)
+    t.end()
+
+
+
+if __name__ == "__main__":
+    main()
+
+
+
--- timer.py
+++ timer.py
@@ -1,2 +1,36 @@
+import time
+
 class Timer(object):
+    def __init__(self):
+        self._start = None
+        self._end = None
+        self._config = 'seconds'
+        self._lastTime = None
     
+    def start(self):
+        if self._start is None:
+            self._start = time.time()
+        else:
+            print 'The timer was already started'
+    
+    def end(self):
+        if self._start is None:
+            print 'The timer was never started'
+        else:
+            self._end = time.time()
+            self._lastTime = self._end - self._start
+            self._start, self._end = None, None
+            self.displayTime(self._lastTime)
+    
+    def configure(self, option):
+        self._config = option
+
+    def displayTime(self, t):
+        if self._config == 'seconds':
+            print self._lastTime, 'seconds'
+        elif self._config == 'minutes':
+            print self._lastTime / 60, 'minutes'
+        elif self._config == 'hours':
+            print self._lastTime / 3600, 'hours'
+        
+
